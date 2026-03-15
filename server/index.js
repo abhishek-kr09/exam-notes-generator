85bfd46b6510ece4979f1112bcc9f9ddc46a0827
abhishekkr1818 <abhishek.kumar.iotcs27@heritageit.edu.in>
2026-03-15 22:28:48 +0530

    initial project setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./utils/connectDB.js";
+dotenv.config()
+
+const app=express()
+
+const PORT=process.env.PORT || 5000;
+app.get("/",(req,res)=>{
+    res.json({message: "Exam notes AI Backend Running"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+    connectDB()
+})
