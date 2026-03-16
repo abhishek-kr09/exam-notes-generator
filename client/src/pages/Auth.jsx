3fa34a4f72729e1a562a008c993f6445ec7699ff
abhishekkr1818 <abhishek.kumar.iotcs27@heritageit.edu.in>
2026-03-16 23:37:07 +0530

    Navbar completed

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -5,9 +5,11 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../utils/firebase";
 import axios from "axios"
 import { serverUrl } from "../App";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../redux/userSlice";
 
 function Auth() {
-
+  const dispatch=useDispatch()
   const handleGoogleAuth=async ()=>{
     try {
       const response=await signInWithPopup(auth,provider)
@@ -17,7 +19,7 @@ function Auth() {
       const result=await axios.post(serverUrl+ "/api/auth/google", {name,email},{
         withCredentials: true
       })
-      console.log(result.data)
+      dispatch(setUserData(result.data))
     } catch (error) {
       console.log(error)
     }
