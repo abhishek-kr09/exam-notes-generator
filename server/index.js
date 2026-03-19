975bebb9f60bee7223ffba1c13e2acd2ec626986
Abhishek kumar <163195949+abhishek-kr09@users.noreply.github.com>
2026-03-19 06:15:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ stripeWebhook
 
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://exam-notes-generator-client.onrender.com",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
